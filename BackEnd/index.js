64f21b9341b31f579143e1e5d7c5d7911f1176be
ELzahraaeid <alzahraaeid62@gmail.com>
2022-06-10 23:35:25 +0200

    whatsapp app

---
+++ BackEnd/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+let onlineUsersForNewUser = []
+io.on('connection', (socket)=>{
+    
+    
+    socket.on('newUser', (userName)=> {
+        const user = {username: userName, id: socket.id}
+        socket.broadcast.emit('new-user', user)
+        io.to(socket.id).emit('online-users', onlineUsersForNewUser)
+        onlineUsersForNewUser.push(user)
+
+    });
+    socket.on('new-message', (payload) =>{
+       
+        io.to(payload.id).emit('new-message', {id:socket.id, message:payload.message})
+           
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('delete-user', socket.id);
+        onlineUsersForNewUser = onlineUsersForNewUser.filter((user => user.id !== socket.id))
+    });
+});
+
+
+server.listen(5000, ()=> {
+    console.log('listen on : 5000')
+    
+})
